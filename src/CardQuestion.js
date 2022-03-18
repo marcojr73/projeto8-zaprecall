615b4482cd8f1e906adaf755e61296aa4def0671
marco junior <marcojuniorengineer@gmail.com>
2022-03-18 09:37:14 -0300

    cards semi-funcionais

--- src/CardQuestion.js
+++ src/CardQuestion.js
@@ -2,14 +2,14 @@ import { useState } from "react";
 import Welcome from "./Welcome";
 
 const deck = [
-    { questionsDeck: "Nome do professor?", answers: "Emmett Brown" },
-    { questionsDeck: "Modelo do veiculo/máquina do tempo", answers: "DeLorean" },
-    { questionsDeck: "Data que Martin chega no futuro no segundo filme", answers: "21 de outubro" },
-    { questionsDeck: "Marca da cueca de Martin?", answers: "Calvin Klein" },
-    { questionsDeck: "Velocidade necessária para se utilizar a máquina do tempo?", answers: "88 milhas p hora" },
-    { questionsDeck: "Nome do cachorro do professor?", answers: "Einstein" },
-    { questionsDeck: "Esporte preferido de martin?", answers: "Skate" },
-    { questionsDeck: "Acidente natural que permite martin voltar para o futuro?", answers: "Raio na torre" }
+    { questionNumber: "pergunta 1", questionsText: "Nome do professor?", answers: "Emmett Brown" },
+    { questionNumber: "pergunta 2", questionsText: "Modelo do veiculo/máquina do tempo", answers: "DeLorean" },
+    { questionNumber: "pergunta 3", questionsText: "Data que Martin chega no futuro no segundo filme", answers: "21 de outubro" },
+    { questionNumber: "pergunta 4", questionsText: "Marca da cueca de Martin?", answers: "Calvin Klein" },
+    { questionNumber: "pergunta 5", questionsText: "Velocidade necessária para se utilizar a máquina do tempo?", answers: "88 milhas p hora" },
+    { questionNumber: "pergunta 6", questionsText: "Nome do cachorro do professor?", answers: "Einstein" },
+    { questionNumber: "pergunta 7", questionsText: "Esporte preferido de martin?", answers: "Skate" },
+    { questionNumber: "pergunta 8", questionsText: "Acidente natural que permite martin voltar para o futuro?", answers: "Raio na torre" }
 ]
 
 const questions = ["pergunta 1", "pergunta 2", "pergunta 3", "pergunta 4", "pergunta 5", "pergunta 6", "pergunta 7", "pergunta 8"];
@@ -20,7 +20,6 @@ const deckrandom = deck.sort(() => {
 
 
 export default function CardQuestion() {
-
     const [visible, setVisible] = useState(true);
 
     return (
@@ -42,44 +41,35 @@ function Header() {
     )
 }
 
-function Question(props) {
-    const [visible, setVisible] = useState(true);
+function Question() {
+    const [select, setSelect] = useState(false);
+
     return (
         <>
-            {questions.map(question => {
+            {deck.map(question => {
                 return (
-                    visible === true ?
-                        <div className="questions">
-                            <p>{question}</p>
-                            <ion-icon onClick={() => setVisible(false)} name="play-outline"></ion-icon>
+                    select === false ?
+                        <div onClick={() => setSelect(true)} className="questions">
+                            <p>{question.questionNumber}</p>
+                            <ion-icon name="play-outline"></ion-icon>
                         </div>
-                        :<section className="card">
+                        : <section className="card">
                             <div className="question face">
-                                <p>{deck.questionsDeck}</p>
+                                <p>{question.questionsText}</p>
+                                <img src="./assets/reverse.png"/>
                             </div>
                             <div className="answer face">
-                                <p>{deck.answers}</p>
+                                <p>{question.answers}</p>
+                                <div className="options">
+                                    <div className="box wrong">Não lemprei</div>
+                                    <div className="box almost">Quase Não lembrei</div>
+                                    <div className="box zap">Zap</div>
+                                </div>
                             </div>
-                         </section>
+                        </section>
                 )
             })}
         </>
     )
 }
 
-// function Card(props) {
-
-
-//     return (
-//         visible === true ?
-//             <section className="card">
-//                 <div className="question face">
-//                     <p>{props.deck.questionsDeck}</p>
-//                 </div>
-//                 <div className="answer face">
-//                     <p>{props.deck.answers}</p>
-//                 </div>
-//             </section>
-//             : <></>
-//     )
-// }
